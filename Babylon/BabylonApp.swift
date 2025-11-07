0dbc74d1cfc352ace80729c9abb11911107ea157
Rutayisire Lucien <rutayisirelucien@gmail.com>
2025-11-06 19:39:33 -0500

    Initial Commit

---
+++ Babylon/BabylonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabylonApp.swift
+//  Babylon
+//
+//  Created by user278698 on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BabylonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
